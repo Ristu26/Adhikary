3c1fa1dba357bab4166757fbd4a4bbc2e837157d
Ristu26 <98215913+Ristu26@users.noreply.github.com>
2022-02-02 13:27:24 +0530

    Create Find max element .cpp

---
+++ Array/Find max element .cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int arr[]={14,22,71,51,46,29,24,2};
+   int max=arr[0];
+   int a=0;
+   for(int i=0;i<sizeof(arr)/sizeof(int);i++)
+   {
+    if(max<arr[i]){
+    max=arr[i];
+    a = i;
+    }
+    }
+    cout << max << endl << a << endl;
+    return 0;
+}
