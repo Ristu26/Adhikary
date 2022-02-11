4e03ff9310f473ae4a509e99fc10a0f3351ce4be
Ristu26 <98215913+Ristu26@users.noreply.github.com>
2022-02-11 20:40:47 +0530

    Create Leap year or not .cpp

---
+++ Number/Leap year or not .cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int year;
+  cout<<"Enter a year:";
+  cin>>year;
+  if(year%4==0)
+  {
+   cout<<"It is a leap year:"<<year;
+}
+else
+{
+  cout<<"It is not a leap year:";
+}
+return 0;
+}
