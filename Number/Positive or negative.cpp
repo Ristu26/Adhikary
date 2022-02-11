77b202feedf41e43b55b4e7fd974453321418e34
Ristu26 <98215913+Ristu26@users.noreply.github.com>
2022-02-11 20:26:53 +0530

    Create Positive or negative.cpp

---
+++ Number/Positive or negative.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a;
+  cin>>a;
+  if(a==0)
+  {
+    cout<<"The number is 0";
+  }
+  else if(a>0)
+  {
+    cout<<"The number is positive";
+  }
+  else
+  {
+    cout<<"The number is negative";
+  }
+  return 0;
+}
