e3a9b3783efea3c692af1826fd368455d549eaba
Ristu26 <98215913+Ristu26@users.noreply.github.com>
2022-02-11 21:36:23 +0530

    Create Sum of AP series.cpp

---
+++ Number/Sum of AP series.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+ int n,a,d,k,q;
+ int sum;
+ cout<<"enter the 1st number of the series";
+ cin>>a;
+ cout<<"enter total number of the series";
+ cin>>n;
+ cout<<"enter common difference";
+ cin>>d;
+ sum=(n*(2*a+(n-1)*d))/2;
+ k*a+(n-1)*d;
+ cout<<"The sum of the AP series"<<endl;
+ for(k*a;k<=q;k=k+d)
+ {
+   if(k!=q)
+   cout<<k<<"+";
+   else
+cout<<sum<<endl;
+ }
+ return 0;
+}
