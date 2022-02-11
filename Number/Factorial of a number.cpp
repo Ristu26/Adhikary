9ba4346fddec8b534ad24c07e215f77f9375e3f9
Ristu26 <98215913+Ristu26@users.noreply.github.com>
2022-02-11 20:43:49 +0530

    Create Factorial of a number.cpp

---
+++ Number/Factorial of a number.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int number;
+  int factorial=1;
+  cout<<"Enter the number:";
+  cin>>number;
+  if(number<0)
+  {
+    cout<<"don't have the factorial for negetive number";
+  }
+  else if(number<=1)
+  {
+    cout<<number<<"!="<<factorial;
+  }
+  else
+{
+  for(int i=number;i>=2;i--)
+  {
+    factorial =factorial*i;
+  }
+  cout<<number<<"!="<<factorial;
+}
+return 0;
+}
