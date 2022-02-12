b82f700a3cab61875867c02c72307925b61f23af
Ristu26 <98215913+Ristu26@users.noreply.github.com>
2022-02-12 12:34:00 +0530

    Create Decimal to binary.cpp

---
+++ Number system/Decimal to binary.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int  a;
+    cout << "The number in decimal ";
+    cin>>a;
+    int c = 0;
+    int i = 0;
+    while(a>0)
+    {
+        c = c + ((a%2)*pow(10,i));
+        a=a/2;
+        i++;
+    }
+    cout <<"The Number in Binary " << c;
+    return 0;
+}
