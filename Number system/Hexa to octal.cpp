559746555971d77c925c439d14181c807cf8f074
Ristu26 <98215913+Ristu26@users.noreply.github.com>
2022-02-17 13:48:15 +0530

    Create Hexa to octal.cpp

---
+++ Number system/Hexa to octal.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+   int dec=0,rem,i=0,len=0;
+    char hex[20];
+    cout<<"Enter the Hexadecimal Number:";
+    cin>>hex;
+    while(hex[i]!=0)
+    {
+    len++;
+    i++;
+    }
+    len--;
+    i=0;
+    while(len>=0)
+    {
+    rem=hex[len];
+    if(rem>=48 && rem<=57)
+    rem = rem-48;
+    else if(rem>=65 && rem<=70)
+    rem = rem-55;
+    else if(rem>=97 && rem<=102)
+    rem = rem-87;
+    else
+    {
+    cout<<"invalid"<<endl;
+    break;
+     }
+    dec=dec+(rem*pow(16,i));
+    len--;
+    i++;
+    }
+    cout<<"decimal number is:"<<dec<<endl;
+    long c=0;
+   rem=0;
+   i=1;
+    while(dec>0)
+    {
+      rem=dec%8;
+        c=c+(rem*i);
+        dec=dec/8;
+        i=i*10;
+    }
+    cout <<"The Number in octal:"<< c;
+    return 0;
+}
