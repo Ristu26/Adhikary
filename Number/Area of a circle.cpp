76f2f275e3cc59a41d4cfa6f59ef145be2693889
Ristu26 <98215913+Ristu26@users.noreply.github.com>
2022-02-20 20:37:19 +0530

    Create Area of a circle.cpp

---
+++ Number/Area of a circle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   float pi=3.14;
+    float r,a;
+   cout<<"Enter the radius of the circle:";
+   cin>>r;
+   a=r*r*pi;
+   cout<<"area of the circle is:"<<a;
+   return 0;
+}
